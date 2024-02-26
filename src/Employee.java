80878dbe46b6950243794eff842010e62ff97317
hardw <hardwoker2017@yandex.ru>
2024-02-27 01:29:13 +0500

    initial commit

---
+++ src/Employee.java
@@ -0,0 +1,60 @@
+import java.util.Objects;
+
+public class Employee {
+    private String fullName;
+    private int departmentNumber;
+    private double salary;
+    private Integer id;
+    static int counter = 1;
+
+
+    public Employee(Integer id, String fullName, int departmentNumber, double salary) {
+        this.fullName = fullName;
+        this.departmentNumber = departmentNumber;
+        this.salary = salary;
+        this.id = counter;
+        counter++;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getDepartmentNumber() {
+        return departmentNumber;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setDepartmentNumber(int departmentNumber) {
+        this.departmentNumber = departmentNumber;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return departmentNumber == employee.departmentNumber && Double.compare(salary, employee.salary) == 0 && Objects.equals(fullName, employee.fullName) && Objects.equals(id, employee.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fullName, departmentNumber, salary, id);
+    }
+
+    @Override
+    public String toString() {
+        return id + " " + fullName + " " + departmentNumber + " " + salary;
+    }
+}
